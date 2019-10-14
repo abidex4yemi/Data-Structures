bc46fc7f655660735c1be80d80aa5fbfa0e1b4a6
abidex4yemi <abidex4yemi@gmail.com>
2019-10-14 15:21:18 +0100

    Complete Stack implementation

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -26,22 +26,45 @@ class Stack:
     def push(self, value):
         if self.head is None:
             self.head = Node(value)
+            self.size += 1
         else:
             new_node = Node(value)
             self.head.prev = new_node
             new_node.next = self.head
             new_node.prev = None
             self.head = new_node
+            self.size += 1
 
     def pop(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            removedvalue = self.head.data
+            self.head = self.head.next
+            if self.size == 1:
+                self.head = None
+            else:
+                self.head.prev is None
+            self.size -= 1
+
+            return removedvalue
 
     def len(self):
-        pass
+        return self.size
 
 
-Node(1)
-Node(1)
-Node(1)
+s = Stack()
+s.push(100)
+# s.push(105)
+# s.push(110)
 
-print(Node(1).data)
+print(s.pop())
+# print(s.pop())
+# print(s.pop())
+
+print(s.len())
+
+# s.push(101)
+# s.push(105)
+# self.s.pop(), 105
+# self.s.len()
--- queue_and_stack/tempCodeRunnerFile.py
+++ queue_and_stack/tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-from doubly_linked_list import DoublyLinkedList
-# import sys
-# sys.path.append('../doubly_linked_list')
+print(s.pop())
+# print(s.pop())
